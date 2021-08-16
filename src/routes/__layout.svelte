993c8f7a66bd70c75515b10bcffc5af495e3c047
Reinaldy Rafli <aldy505@tutanota.com>
2021-08-16 18:38:07 +0700

    feat: forgot to include font

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-  class="bg-gradient-to-r from-rose-100 to-teal-100 text-black dark:(bg-gradient-to-r from-gray-700 via-gray-900 to-black text-light-100) min-h-screen h-full w-full"
+  class="font-body bg-gradient-to-r from-rose-100 to-teal-100 text-black dark:(bg-gradient-to-r from-gray-700 via-gray-900 to-black text-light-100) min-h-screen h-full w-full"
 >
   <div class="container mx-auto lg:px-48 md:px-32 sm:px-20 px-8 py-8">
     <Navbar />
