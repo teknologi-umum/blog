52881ef15f51521f596871e9a7d406f3921bf32c
elianiva <dicha.arkana03@gmail.com>
2021-08-18 16:44:32 +0700

    [WIP]: Wed Aug 18 04:44:32 PM WIB 2021

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  distDir: 'static',
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'github.com', 'avatars.githubusercontent.com'],
