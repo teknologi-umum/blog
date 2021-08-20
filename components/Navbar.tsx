74de6f3c9bb0223e17aea4f45ff813d1746cfeb4
Reinaldy Rafli <aldy505@tutanota.com>
2021-08-20 13:44:24 +0700

    Revert "Merge pull request #11 from teknologi-umum/separate-content"
    This reverts commit 6ea82c5b76efbaeb351dce107d029ef87f51d471, reversing
    changes made to 5f2f115a047166a2571c41985e370d23e12c8207.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-20 h-16 backdrop-blur-sm">
-      <div className="flex items-center h-full max-w-screen-lg px-4 mx-auto">
+    <nav className="fixed h-16 top-0 left-0 right-0 backdrop-blur-sm z-20">
+      <div className="flex items-center px-4 mx-auto max-w-screen-lg h-full">
         <Link href="/">
-          <a className="flex-1 text-lg font-bold">Teknologi Umum</a>
+          <a className="flex-1 font-bold text-lg">Teknologi Umum</a>
         </Link>
         <div className="flex gap-4">
-          {['home', 'blogs', 'news', 'about'].map((route, idx) => (
+          {['home', 'blog', 'about'].map((route, idx) => (
             <Link href={`/${route === 'home' ? '' : route}`} key={`${idx}-${route}`}>
-              <a className="flex-1 text-lg text-gray-700 capitalize hover:text-blue-600 hover:underline">{route}</a>
+              <a className="flex-1 text-lg capitalize text-gray-700 hover:text-blue-600 hover:underline">{route}</a>
             </Link>
           ))}
         </div>
--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 
-export default function ContentPreview({ slug, title, description, categories, author, github }) {
+export default function PostCard({ slug, title, desc, categories, author, github }) {
   return (
-    <div className="flex flex-col overflow-hidden rounded-md shadow-lg">
+    <div className="flex flex-col shadow-lg rounded-md overflow-hidden">
       <img
         className="h-[10rem] object-cover"
         src="https://images.unsplash.com/photo-1549880181-56a44cf4a9a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
@@ -12,24 +12,23 @@ export default function ContentPreview({ slug, title, description, categories, a
       />
       <div className="p-4">
         <div className="flex gap-1 mb-2">
-          {categories &&
-            categories.map((category: string, idx: number) => (
-              <span
-                className="px-2 text-xs font-semibold text-gray-700 uppercase bg-gray-200 rounded-sm"
-                key={`${idx}-${category}`}
-              >
-                {category}
-              </span>
-            ))}
+          {categories.map((category: string, idx: number) => (
+            <span
+              className="text-xs px-2 rounded-sm bg-gray-200 text-gray-700 uppercase font-semibold"
+              key={`${idx}-${category}`}
+            >
+              {category}
+            </span>
+          ))}
         </div>
         <Link href={`/posts/${slug}`}>
-          <a className="text-lg font-bold text-gray-800 capitalize font-display hover:text-blue-600 hover:underline">
+          <a className="text-lg font-bold capitalize font-display text-gray-800 hover:text-blue-600 hover:underline">
             {title}
           </a>
         </Link>
-        <p className="mb-2 text-sm leading-relaxed text-gray-500">{description}</p>
+        <p className="text-sm leading-relaxed text-gray-500 mb-2">{desc}</p>
         <a className="group" href={`https://github.com/${github}`}>
-          <div className="flex items-center justify-start gap-2">
+          <div className="flex gap-2 items-center justify-start">
             <img
               className="rounded-full shadow-md"
               src={`https://github.com/${github}.png`}
