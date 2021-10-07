fd91b79dff0006e93040e6d1011c82d3e5289173
arnoldart <adoni.091203@gmail.com>
2021-10-07 19:46:10 +0700

    fix FeaturedPost

--- components/FeaturedPost.tsx
+++ components/FeaturedPost.tsx
@@ -1,38 +1,35 @@
 import Link from 'next/link';
 
-const FeaturedPost = ({ posts }) => {
-  return posts.slice(0, 1).map(({ title, author, desc }) => (
-    <div
-      key={title}
-      className="grid lg:grid-cols-2 grid-cols-1 items-center mt-12 md:-mx-12 lg:gap-x-20 gap-y-10 lg:gap-y-0"
-    >
+const FeaturedPost = ({ post }) => {
+  return (
+    <div className="grid lg:grid-cols-2 grid-cols-1 items-center mt-12 md:-mx-12 lg:gap-x-20 gap-y-10 lg:gap-y-0">
       <div className="lg:text-left text-center">
         <p className="text-black font-medium">FEATURED</p>
-        <h1 className="sm:text-5xl text-4xl mb-4 mt-2 font-bold text-black">{title}</h1>
-        <a className="group inline-block" href={`https://github.com/`}>
+        <h1 className="sm:text-5xl text-4xl mb-4 mt-2 font-bold text-black">{post.title}</h1>
+        <a className="group inline-block" href={`https://github.com/${post.github}`}>
           <div className="flex space-x-2 items-center justify-start">
             <img
               className="rounded-full shadow-md"
-              src={`https://avatars.githubusercontent.com/u/1024025?v=4`}
+              src={`https://github.com/${post.github}.png`}
               width="32"
               height="32"
-              alt="author"
+              alt={post.github}
             />
-            <span className="text-sm text-left text-gray-700 group-hover:text-primary-600">{author}</span>
+            <span className="text-sm text-left text-gray-700 group-hover:text-primary-600">{post.author}</span>
           </div>
         </a>
-        <p className="mt-4 leading-relaxed font-serif text-gray-500">{desc}</p>
+        <p className="mt-4 leading-relaxed font-serif text-gray-500">{post.desc}</p>
         <Link href="/blog">
           <a className="bg-primary-900 text-white py-1.5 px-10 inline-block mt-4">READ MORE</a>
         </Link>
       </div>
       <div className="w-5/6 lg:w-full mx-auto lg:mx-0">
         <div className="aspect-w-16 aspect-h-13 w-full">
-          <img className="w-full object-cover object-center" src="https://picsum.photos/500" alt={title} />
+          <img className="w-full object-cover object-center" src="https://picsum.photos/500" alt={post.title} />
         </div>
       </div>
     </div>
-  ));
+  );
 };
 
 export default FeaturedPost;
