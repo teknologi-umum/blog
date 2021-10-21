417b12ee59e2a7072cfaddcd1e8eb5f39cf3d6fc
Opa Kholis Majid <opakholis.id@gmail.com>
2021-10-22 05:47:08 +0700

    refactor: make the color lighter when hover

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,13 +13,13 @@ export default function Navbar() {
     return router.route === `/${route}` ? 'active' : '';
   };
   return (
-    <nav className="fixed min-h-16 top-0 inset-x-0 bg-white/80 backdrop-filter backdrop-blur-lg z-20 font-sans">
+    <nav className="fixed min-h-16 top-0 inset-x-0 bg-white bg-opacity-80 backdrop-filter backdrop-blur-lg z-20 font-sans">
       <div className="container flex flex-col md:flex-row justify-between items-center space-y-1 mx-auto py-5 px-8 sm:px-16 md:px-32 lg:px-40 xl:px-56 2xl:px-72 h-full">
         <div className="flex-1 space-x-6">
           {['home', 'blog', 'about'].map((route, idx) => (
             <Link href={`/${route === 'home' ? '' : route}`} key={`${idx}-${route}`}>
               <a
-                className={`flex-inline uppercase text-center text-gray-700 hover:text-primary-600 transition duration-300 ${active(
+                className={`flex-inline uppercase text-center hover:text-primary-600 transition duration-300 ${active(
                   route,
                   route === 'home',
                 )}`}
