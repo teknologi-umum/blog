e5f64d0304a938ad98dd8b7f893a2e2b00c518f0
elianiva <dicha.arkana03@gmail.com>
2022-10-01 10:22:45 +0700

    feat: toggle more/less topics

--- components/BrowseTopic.tsx
+++ components/BrowseTopic.tsx
@@ -1,17 +1,22 @@
 import Link from 'next/link';
+import { useReducer } from 'react';
+
+const SHOWN_TOPICS_LIMIT = 15;
 
 type BrowseTopicProps = {
   categories: string[];
 };
 
 export function BrowseTopic({ categories }: BrowseTopicProps) {
+  const [showMoreTopics, toggleShownTopics] = useReducer((s) => !s, false);
+
   return (
     <>
       <div className="md:-mx-12 mt-24">
         <h2 className="uppercase font-bold text-xl mb-10">Browse Interesting Topics</h2>
 
-        <div className="flex flex-wrap gap-6 items-center">
-          {categories.map((category: string, i: number) => (
+        <div className="flex flex-wrap gap-6 items-center justify-center">
+          {categories.slice(0, showMoreTopics ? -1 : SHOWN_TOPICS_LIMIT).map((category: string, i: number) => (
             <Link
               key={i}
               href={{
@@ -25,6 +30,13 @@ export function BrowseTopic({ categories }: BrowseTopicProps) {
             </Link>
           ))}
         </div>
+
+        <button
+          className="block mx-auto mt-10 font-medium text-lg text-gray-600 hover:text-primary-600 hover:underline decoration-dashed underline-offset-4"
+          onClick={() => toggleShownTopics()}
+        >
+          SHOW {showMoreTopics ? 'LESS' : 'MORE'} TOPICS
+        </button>
       </div>
     </>
   );
